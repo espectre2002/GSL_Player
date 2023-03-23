4deb5704b30bd1eb943092922e13fedad2e532c5
espectre2002 <bl8067953@gmail.com>
2023-03-23 12:51:07 -0600

    correcion

--- app/src/main/java/com/example/gslplayer/MainActivity.java
+++ app/src/main/java/com/example/gslplayer/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         btnentrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, Home.class));
+
             }
         });
 
