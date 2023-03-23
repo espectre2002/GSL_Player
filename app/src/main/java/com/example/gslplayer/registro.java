0e70e04433345b452adb4cfb22c3fbce71cb98f4
espectre2002 <bl8067953@gmail.com>
2023-03-23 13:19:31 -0600

    hola

--- app/src/main/java/com/example/gslplayer/registro.java
+++ app/src/main/java/com/example/gslplayer/registro.java
@@ -2,13 +2,26 @@ package com.example.gslplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class registro extends AppCompatActivity {
+    TextView txtregistrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+        txtregistrar = findViewById(R.id.txtregistrar);
+
+        txtregistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(registro.this,MainActivity.class));
+
+            }
+        });
     }
 }
